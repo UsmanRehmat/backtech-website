8b49357a3cbc7b0a711d6dce21f120a371383fa6
Abdul Hanan <abhanan369@gmail.com>
2024-01-01 15:30:58 +0500

    added netlify file

--- src/app/terms/and/conditions/page.tsx
+++ src/app/terms/and/conditions/page.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Link from "next/link";
+// import Link from "next/link";
 
 import { InnerMain } from "@components/InnerMain";
 import { TERMS_CONDITIONS_UPDATE } from "@utils/constant";
